58487eaa8370a9bdb3619e3693db8b22d3b6b0c3
Philip <philipsandmann@gmail.com>
2021-12-06 17:42:43 +0100

    rewrote with iterate instead of recursion

--- 06.hs
+++ 06.hs
@@ -5,13 +5,11 @@ main = do
     input <- splitOn "," <$> readFile "inputs/06.txt"
     let fish = map (read::String -> Int) input 
     let fishVector = startVector fish 
-    print $ sum (spawn' fishVector 80)
-    print $ sum (spawn' fishVector 256)
+    print $ sum . (!! 80) $ iterate spawn fishVector
+    print $ sum . (!! 256) $ iterate spawn fishVector
     
 startVector :: [Int] -> [Int]
 startVector l = [0] ++ (map length . group $ sort l) ++ [0,0,0] 
 
-spawn' :: [Int] -> Int -> [Int]
-spawn' (x:xs) n 
-    | n == 0 = (x:xs)
-    | otherwise = spawn' (zipWith (+) (xs ++ [x]) [0,0,0,0,0,0,x,0,0]) (n-1)
+spawn :: [Int] -> [Int]
+spawn (x:xs)  = (zipWith (+) (xs ++ [x]) [0,0,0,0,0,0,x,0,0]) 
